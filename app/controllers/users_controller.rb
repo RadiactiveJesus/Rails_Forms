7d11825d837b45d77275f33b022d148758279a1c
Noordeen Malango <noordinmalango@gmail.com>
2019-07-23 16:45:58 +0200

    update functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
       redirect_to new_user_path
     else
@@ -13,9 +13,9 @@ class UsersController < ApplicationController
     end
 
   end
-  
+
   def index
-    @users = User.all 
+    @users = User.all
   end
 
   def show
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find params[:id]
     if @user.update_attributes(user_params)
-      redirect_to edit_user_path(@user)
+      redirect_to users_path()
     else
       render :edit
     end
